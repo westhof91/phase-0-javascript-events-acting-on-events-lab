4d9913c65a4f172410e69e69dd883b6685d1a9e5
DeAndre Hoff <deandreryanhoff@yahoo.com>
2021-10-06 19:54:13 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Your code here
+const dodger = document.getElementById('dodger')
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+      console.log(e)
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+
+    } 
+    
+  });
+
+function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10)
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`
+    }
+  }
+  
+  document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft()
+    }
+  })
+  
+  document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight()
+    }
+  })
